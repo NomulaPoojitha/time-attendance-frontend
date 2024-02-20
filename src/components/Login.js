d7a05df566c47e073af96c780a07a5e7d5b582eb
Boro, Nihar Jyoti <nihar.jyoti.boro@ADP.com>
2024-02-20 23:23:02 +0530

    login form update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-export default function Login() {
-  return (
-    <div>Login credentials</div>
+// export default function Login() {
+//   return (
+//     <div>Login credentials</div>
+//   )
+// }
+
+
+const LoginForm = () => {
+  return(
+    <div className='wrapper'>
+      <form action="">
+        <h1>Login</h1>
+        <div className='input-box'>
+          <input type='text' placeholder='Username' required />
+        </div>
+        <div className='input-box'>
+          <input type='password' placeholder='Password' required />
+        </div>
+
+        <button type='submit'>Login</button>
+
+      </form>
+    </div>
   )
 }
+
+export default LoginForm;
